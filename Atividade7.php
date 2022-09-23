f9b3c5ae561e52a271cc52c6eb3bf112da51ba29
Kaue <kaue_lennon@hotmail.com>
2022-09-23 00:11:52 -0400

    Criando codigo IMC

---
+++ Atividade7.php
@@ -0,0 +1,27 @@
+<?php
+
+$dicionario = [
+    "18.5" => "Abaixo do peso",
+    "24.9" => "Peso normal",
+    "29.9" => "Sobrepeso",
+    "34.9" => "Obesidade grau 1",
+    "39.9" => "Obesidade grau 2",
+    "40.0" => "Obesidade grau 3"
+];
+
+
+echo "Teste";
+
+foreach($dicionario as $key => $value){
+    //Se a chave for menor que 40, então testa os valores de acordo com o IMC. 
+    if((float) $key < 40.0){
+        if($imc <= (float) $key){
+            echo PHP_EOL."$value";
+            break;
+        }
+    }else{ //Se o IMC for maior ou igual a 40
+        echo "Seu peso é preocupante, procure um médico".PHP_EOL."$value";
+    }
+}
+
+?>
